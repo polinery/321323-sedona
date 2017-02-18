4dc79cc284a2d2edfb5e4639de0a0681d5e53c5d
polinery <polinery@bk.ru>
2017-02-18 18:11:08 +0300

    Подключили JS

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var open = document.querySelector(".open-form");
+var popup = document.querySelector(".search-form");
+open.addEventListener("click", function(){
+  popup.classList.toggle("popup-hide");
+  popup.classList.toggle("popup-show");
+});
