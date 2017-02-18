98d85c26c395359e5b51182006efe50c9e6293da
polinery <polinery@bk.ru>
2017-02-18 23:11:12 +0300

    Добавлена интерактивная карта

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+document.body.classList.remove("no-js")
 var open = document.querySelector(".open-form");
 var popup = document.querySelector(".search-form");
-open.addEventListener("click", function(){
+open.addEventListener("click", function () {
   popup.classList.toggle("popup-hide");
   popup.classList.toggle("popup-show");
 });
